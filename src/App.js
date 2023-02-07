49216d62e562867fd5e08394655eff22c486d59c
seoyeon-double-7 <s2103@e-mirim.hs.kr>
2023-02-07 11:40:33 +0900

    Add build folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
-function App() {
+const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route
+          path={`/${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
